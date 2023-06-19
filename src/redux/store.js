880be2372b943fa0bf8d101c494a16c798823b1a
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-19 08:14:34 +0300

    Add profileSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import authSlice from './authSlice'
+import profileSlice from './profile/profileSlice'
 
 const reducer = combineReducers({
+     auth: authSlice,
+     profile: profileSlice
+
 })
 
-export const store = configureStore({reducer})
+export const store = configureStore({ reducer })
