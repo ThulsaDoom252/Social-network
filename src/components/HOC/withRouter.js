819e3c23d3a898ceea4d0039dffe2190938fc86a
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-19 20:33:43 +0300

    Integrate withRouter and authHoc for enhanced routing and authentication

---
+++ src/components/HOC/withRouter.js
@@ -0,0 +1,20 @@
+import {useLocation, useNavigate, useParams} from "react-router";
+import React from "react";
+
+const withRouter = (Component) => {
+     function ComponentWithRouterProp(props) {
+          const location = useLocation();
+          const navigate = useNavigate();
+          const  params = useParams();
+          return (
+               <Component
+                    {...props}
+                    router={{location, navigate, params}}
+               />
+          );
+     }
+
+     return ComponentWithRouterProp;
+}
+
+export default withRouter
