b1a76efd502c74c41ecbfaf5d7d125dc3d4d9c85
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-21 21:12:41 +0300

    Version changed to 2.0, implement changes in Readme and About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
           <div className='about-page'>
                <div className='content'>
                     <h1 className='title'>Samurai's social network</h1>
-                    <p className='version'>Current Version: 1.8</p>
+                    <p className='version'>Current Version: 2.0</p>
                     <p className='developer'>Developer: ThulsaDoom</p>
                     <div className='functionality'>
                          <h3 className='functionality-title'>Functionality:</h3>
@@ -26,6 +26,8 @@ const About = () => {
                               <li className='functionality-item'> Edit personal information (directly on profile page or
                                    at separate edit-profile page)>
                               </li>
+                              <li className='functionality-item'>Edit contacts directly from profile page
+                              </li>
                          </ul>
                     </div>
                     <div className='user-edit-data'>
@@ -35,6 +37,7 @@ const About = () => {
                               <li className='functionality-item'>User-name</li>
                               <li className='functionality-item'>Is user looking for a job or not</li>
                               <li className='functionality-item'>Skills/desired job description</li>
+                              <li className='functionality-item'>Contacts</li>
                          </ul>
                     </div>
                </div>
