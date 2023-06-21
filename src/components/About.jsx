b29e663a17615b7598d8ebe496e410acf0e331d9
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-21 18:37:23 +0300

    Refactor About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const About = () => {
      return (
@@ -9,30 +9,35 @@ const About = () => {
                     <p className='developer'>Developer: ThulsaDoom</p>
                     <div className='functionality'>
                          <h3 className='functionality-title'>Functionality:</h3>
-                         <ul>
-                              <li>User list browsing</li>
-                              <li>Registration and login</li>
-                              <li>Night mode</li>
-                              <li>Preview of mobile version</li>
-                              <li>Add/Remove Friends</li>
-                              <li>Messages (non-functioning, static-only)</li>
-                              <li>Edit Status</li>
-                              <li>User can be followed/unfollowed directly from the profile page.</li>
-                              <li>Add static user photos (not editable, can't be removed or added).</li>
+                         <ul className={'functionality-list'}>
+                              <li className='functionality-item'>User list browsing</li>
+                              <li className='functionality-item'>Registration and login</li>
+                              <li className='functionality-item'>Night mode</li>
+                              <li className='functionality-item'>Preview of mobile version</li>
+                              <li className='functionality-item'>Add/Remove Friends</li>
+                              <li className='functionality-item'>Messages (non-functioning, static-only)</li>
+                              <li className='functionality-item'>Edit Status</li>
+                              <li className='functionality-item'>User can be followed/unfollowed directly from the
+                                   profile page.
+                              </li>
+                              <li className='functionality-item'>Add static user photos (not editable, can't be removed
+                                   or added).
+                              </li>
                          </ul>
                     </div>
                     <div className='user-edit-data'>
-                         <h4 className='edit-data-title'>User can edit the following data (only in direct edit mode):</h4>
-                         <ul>
-                              <li>Personal info</li>
-                              <li>User-name</li>
-                              <li>Is user looking for a job or not</li>
-                              <li>Skills/desired job description</li>
+                         <h4 className='edit-data-title'>User can edit the following data (only in direct edit
+                              mode):</h4>
+                         <ul className={'functionality-list'}>
+                              <li className='functionality-item'>Personal info</li>
+                              <li className='functionality-item'>User-name</li>
+                              <li className='functionality-item'>Is user looking for a job or not</li>
+                              <li className='functionality-item'>Skills/desired job description</li>
                          </ul>
                     </div>
                </div>
           </div>
-     );
-};
+     )
+}
 
-export default About;
+export default About
