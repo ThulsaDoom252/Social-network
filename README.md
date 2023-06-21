6806da5af8976e5184b359bd34d3ea8f43c845f7
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-21 18:34:29 +0300

    Refactor About component

--- README.md
+++ README.md
@@ -11,4 +11,17 @@ Functionality:
 - Night mode
 - Preview of mobile version
 - Add/Remove Friends
-- Messages (non-functioning)
+- Messages (non-functioning, static-only)
+- Edit Status
+- User can be followed/unfollowed directly from the profile page.
+- Add static user photos (not editable, can't be removed or added).
+
+#### User can edit following data (only in direct edit mode):
+- Personal info
+- User-name
+- Is user looking for a job or not
+- Skills/desired job description
+
+
+
+
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,59 +1,38 @@
 import React from 'react';
-import authHoc from './HOC/authHoc';
-import { connect } from 'react-redux';
 
 const About = () => {
      return (
           <div className='about-page'>
                <div className='content'>
-                    <h1 className='title'>Samurai's Social Network</h1>
-                    <p className='developer'>Developer: ThulsaDoom</p>
+                    <h1 className='title'>Samurai's social network v1.5</h1>
                     <p className='version'>Current Version: 1.5</p>
-                    <p className='functionality'>
-                         Functionality:
-                         <br />
-                         - User list browsing
-                         <br />
-                         - Registration and login
-                         <br />
-                         - Night mode
-                         <br />
-                         - Preview of mobile version
-                         <br />
-                         - Add/Remove friends
-                         <br />
-                         - Messages (non-functioning)
-                         <br />
-                    </p>
-                    <p className='user-edit-data'>
-                         User can edit the following data (only if direct edit mode is on):
-                         <br />
-                         - Personal info
-                         <br />
-                         - User-name
-                         <br />
-                         - Info: Is user looking for a job or not
-                         <br />
-                         - Info about skills/desired job description
-                         <br />
-                         - Edit current status (not dependent on direct edit mode option)
-                    </p>
-                    <p className='additional-functionality'>
-                         Additionally, the following functionality has been implemented:
-                         <br />
-                         - User can be followed/unfollowed directly from the profile page.
-                         <br />
-                         - Add static user photos (not editable, can't be removed or added).
-                    </p>
+                    <p className='developer'>Developer: ThulsaDoom</p>
+                    <div className='functionality'>
+                         <h3 className='functionality-title'>Functionality:</h3>
+                         <ul>
+                              <li>User list browsing</li>
+                              <li>Registration and login</li>
+                              <li>Night mode</li>
+                              <li>Preview of mobile version</li>
+                              <li>Add/Remove Friends</li>
+                              <li>Messages (non-functioning, static-only)</li>
+                              <li>Edit Status</li>
+                              <li>User can be followed/unfollowed directly from the profile page.</li>
+                              <li>Add static user photos (not editable, can't be removed or added).</li>
+                         </ul>
+                    </div>
+                    <div className='user-edit-data'>
+                         <h4 className='edit-data-title'>User can edit the following data (only in direct edit mode):</h4>
+                         <ul>
+                              <li>Personal info</li>
+                              <li>User-name</li>
+                              <li>Is user looking for a job or not</li>
+                              <li>Skills/desired job description</li>
+                         </ul>
+                    </div>
                </div>
           </div>
      );
 };
 
-const mapStateToProps = state => {
-     return {
-          auth: state.auth.isLogged
-     };
-};
-
-export default connect(mapStateToProps, null)(authHoc(About));
+export default About;
